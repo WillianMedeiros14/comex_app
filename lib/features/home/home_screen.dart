67a569339dbc76ace0003a8b83a5496f9313fb7d
Willian Medeiros <willianmedeiroslm@gmail.com>
2024-11-11 19:59:36 -0300

    feature: add text home

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -17,19 +17,54 @@ class _HomScreenState extends State<HomeScreen> {
             Padding(
               padding: const EdgeInsets.only(
                   left: 16, top: 14, right: 16, bottom: 14),
-              child: Container(
-                width: 73,
-                height: 73,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image:
-                        AssetImage("assets/images/background_onboarding2.png"),
-                    fit: BoxFit.cover,
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  width: 73,
+                  height: 73,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage(
+                          "assets/images/background_onboarding2.png"),
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 16, bottom: 16, right: 16, top: 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RichText(
+                    text: const TextSpan(
+                      style: TextStyle(
+                        fontFamily: "Inter",
+                        fontWeight: FontWeight.w600,
+                        fontSize: 26,
+                        color: Colors.black,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: "Escolha seus ",
+                        ),
+                        TextSpan(
+                          text: "items",
+                          style: TextStyle(color: Colors.red),
+                        ),
+                        TextSpan(
+                          text: " e realize seu pedido",
+                        ),
+                      ],
+                    ),
+                    softWrap: true,
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
