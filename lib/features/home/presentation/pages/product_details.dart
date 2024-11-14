30ce86e3173d0a8688728497ef121e437160cd30
Willian Medeiros <willianmedeiroslm@gmail.com>
2024-11-14 12:00:31 -0400

    feature: creating floating cart button

--- lib/features/home/presentation/pages/product_details.dart
+++ lib/features/home/presentation/pages/product_details.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 import '../widgets/Add_product_bottom.dart';
@@ -11,6 +13,7 @@ class ProductDetails extends StatefulWidget {
 }
 
 class _ProductDetailsState extends State<ProductDetails> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -112,7 +115,79 @@ class _ProductDetailsState extends State<ProductDetails> {
           ),
           ProductDetailsScreenHeader(),
           AddProductBottom(),
+
+          DraggableWidget()
         ],
+      ),
+    );
+  }
+}
+
+
+class DraggableWidget extends StatefulWidget {
+  @override
+  _DraggableWidgetState createState() => _DraggableWidgetState();
+}
+
+class _DraggableWidgetState extends State<DraggableWidget> {
+      double _top = 150;
+  double _left = 16;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Positioned(
+      top: _top,
+      left: _left,
+      child: GestureDetector(
+        onPanUpdate: (details) {
+          setState(() {
+            _top = max(0, _top + details.delta.dy );
+            _left =  max(0, _left +details.delta.dx );
+          });
+        },
+        child: Container(
+          width: 65,
+          height: 65,
+          decoration: const BoxDecoration(
+            color: Color.fromARGB(255, 192, 35, 24),
+            shape: BoxShape.circle,
+          ),
+          child: Stack(
+            children: [
+             
+             const Align(
+                alignment: Alignment.center,
+                child: Icon(
+                  Icons.shopping_cart,
+                  size: 35,
+                  color: Colors.white,
+                ),
+              ),
+            
+              Positioned(
+                top: 0, 
+                right: 0, 
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child:const Text(
+                    '10',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
